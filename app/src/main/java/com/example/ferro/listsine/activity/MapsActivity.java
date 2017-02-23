fed1f41e50bd5f1d97a0183ebccb5975acdb0d4c
ArthurFerrao <arthurdeferrao@gmail.com>
2017-02-23 20:04:40 -0300

    first

--- app/src/main/java/com/example/ferro/listsine/activity/MapsActivity.java
+++ app/src/main/java/com/example/ferro/listsine/activity/MapsActivity.java
@@ -1,10 +1,18 @@
 package com.example.ferro.listsine.activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.ferro.listsine.R;
 import com.example.ferro.listsine.asyncTask.ListAsy;
@@ -23,10 +31,11 @@ import java.lang.reflect.Array;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+    private LocationManager locationManager;
     private GoogleMap mMap;
     String lat, lon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +44,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION},
+                    200);
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                2000,
+                10, this);
+
+
     }
 
 
@@ -50,56 +80,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        try {
-            mMap = googleMap;
-            mMap.setOnMyLocationChangeListener(myLocationChangeListener);
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-
-                return;
-            }
-            mMap.setMyLocationEnabled(true);
-            double latitude, longitude;
-            //LatLng CampinaG = new LatLng(-7.2250861, -35.8811222);
-
-            ListAsyDet listSine = new ListAsyDet();
-            String link = "http://mobile-aceite.tcu.gov.br/mapa-da-saude/rest/emprego/latitude/" + lat + "/longitude/" + lon + "/raio/100";
-            List<SineDet> sines =  listSine.execute(link).get();
-
-            for(int i = 0; i < sines.size(); i ++){
-                latitude = Double.parseDouble(sines.get(i).getLat());
-                longitude = Double.parseDouble(sines.get(i).getLon());
-                mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(latitude, longitude))
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.sine))
-                        .title(sines.get(i).getNome())); // Exibe o nome do posto como titulo do marcador
-
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
-
+        mMap = googleMap;
 
     }
 
 
 
 
-    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
-        @Override
-        public void onMyLocationChange(Location location) {
-            String msg = "New Latitude: " + location.getLatitude()
-                    + "New Longitude: " + location.getLongitude();
-            lat = String.valueOf(location.getLatitude());
-            lon =  String.valueOf(location.getLongitude());
-            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(loc));
-            if(mMap != null){
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
-            }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = Double.toString(location.getLatitude());
+        lon =  Double.toString(location.getLongitude());
+        Log.i("Lat e Lon",lat+" " + lon);
+        LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(loc));
+        if(mMap != null){
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 9.0f));
         }
-    };
+
+        double latitude, longitude;
+        //LatLng CampinaG = new LatLng(-7.2250861, -35.8811222);
+        try {
+        ListAsyDet listSine = new ListAsyDet();
+        String link = "http://mobile-aceite.tcu.gov.br/mapa-da-saude/rest/emprego/latitude/" + lat + "/longitude/" + lon + "/raio/100";
+        List<SineDet> sines = null;
+
+            sines = listSine.execute(link).get();
+
+
+        for(int i = 0; i < sines.size(); i ++){
+            latitude = Double.parseDouble(sines.get(i).getLat());
+            longitude = Double.parseDouble(sines.get(i).getLon());
+            mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(latitude, longitude))
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.sine))
+                    .title(sines.get(i).getNome())); // Exibe o nome do posto como titulo do marcador
+
+        }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+        Toast.makeText(getBaseContext(), "Gps is turned on!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                Toast.LENGTH_SHORT).show();
+    }
 }
